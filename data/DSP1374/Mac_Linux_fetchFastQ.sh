609a6945a9f772716cfcae65c04b852972cc6983
Xavier Castellanos-Girouard <105664447+Xavier-Castellanos-Girouard@users.noreply.github.com>
2025-07-07 15:17:19 -0400

    Add files via upload

---
+++ data/DSP1374/Mac_Linux_fetchFastQ.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+for f in `curl -k "https://genomique.iric.ca/FastQList?key=2409-75d8a78bf3b74bceeefde0b34bf093c6&projectID=1374"`
+do
+  curl -k -o "#1" -C - --create-dirs -O $f
+done
