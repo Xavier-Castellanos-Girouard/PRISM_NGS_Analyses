4ed9212121d49a553d29c34b7925da7d458ace59
Xavier Castellanos-Girouard <105664447+Xavier-Castellanos-Girouard@users.noreply.github.com>
2025-07-07 15:41:04 -0400

    Update Final_Analysis_and_Figure.R

--- scripts/Final_Analysis_and_Figure.R
+++ scripts/Final_Analysis_and_Figure.R
@@ -14,26 +14,15 @@ library(data.table)
 
 #### Import data ####
 
-#TEST <- fread("./Nterm/results/Nterm_mapped_clean.csv")
-
-Nterm_annotated_DF <- fread("./Nterm/results/Nterm_annotated_onTarget.csv")
+Nterm_annotated_DF <- fread("../results/Nterm/results/Nterm_annotated_onTarget.csv")
 Nterm_annotated_DF <- Nterm_annotated_DF[Nterm_annotated_DF$mapping_quality >= 20,]
 Nterm_annotated_DF <- Nterm_annotated_DF %>% select(-c("V1"))
 Nterm_annotated_DF <- Nterm_annotated_DF[(Nterm_annotated_DF$category == "proper_pairs") | (Nterm_annotated_DF$category == "reverse_only")]
 
-Cterm_FullLinker_DF <- fread("./Cterm/results/Cterm_annotated_onTarget.csv")
+Cterm_FullLinker_DF <- fread("../results/Cterm/results/Cterm_annotated_onTarget.csv")
 Cterm_FullLinker_DF <- Cterm_FullLinker_DF[Cterm_FullLinker_DF$mapping_quality >= 20,]
+Nterm_annotated_DF <- Nterm_annotated_DF %>% select(-c("V1"))
 Cterm_FullLinker_DF <- Cterm_FullLinker_DF[(Cterm_FullLinker_DF$category == "proper_pairs") | (Cterm_FullLinker_DF$category == "reverse_only")]
-
-#Nterm_FullLinker_DF <- read.csv("./Nterm/results/Nterm_FrameShift_R1.csv")
-#Cterm_FullLinker_DF <- read.csv("./Cterm/results/Cterm_FrameShift_R1.csv")
-
-# Masih results
-#R1 <- read.csv("/home/xavier/Desktop/Mammalian_CRISPR/PRISM_Nested_PCR/res_n_library/plasmidLinkerPolyGQTrimmed.ReadFiltered.sorted.R1.genome_annotation.tsv", sep = "\t", header = FALSE)
-#R1 <- R1[!(R1$V1 %in% c("__alignment_not_unique", "__ambiguous", "__not_aligned", "__too_low_aQual")),]
-
-#R2 <- read.csv("/home/xavier/Desktop/Mammalian_CRISPR/PRISM_Nested_PCR/res_n_library/plasmidLinkerPolyGQTrimmed.ReadFiltered.sorted.R2.genome_annotation.tsv", sep = "\t", header = FALSE)
-#R2 <- R2[!(R2$V1 %in% c("__alignment_not_unique", "__ambiguous", "__not_aligned", "__no_feature", "__too_low_aQual")),]
 
 
 #### Format Data ####
@@ -67,7 +56,7 @@ Nterm_annotated_reverse_only_DF <-
   Nterm_annotated_DF %>%
   filter(category == "reverse_only")
 
-#### Check on-Target ratio ####
+#### Check on-Target ratios ####
 
 Nterm_FullLink_onTar_DF <-
   Nterm_annotated_proper_pair_DF  %>%
@@ -75,18 +64,7 @@ Nterm_FullLink_onTar_DF <-
   summarize(read_count = n())
 
 Nterm_FullLink_onTar_DF <-
-  Nterm_annotated_DF  %>%
-  filter(category == "proper_pairs") %>%
-  dplyr::group_by(onTarget) %>%
-  summarize(read_count = n())
-
-Nterm_FullLink_onTar_DF <-
-  Nterm_annotated_DF %>%
-  dplyr::group_by(onTarget) %>%
-  summarize(read_count = n())
-
-Nterm_FullLink_onTar_DF <-
-  Nterm_annotated_reverse_only_DF %>%
+  Nterm_annotated_reverse_only_DF  %>%
   dplyr::group_by(onTarget) %>%
   summarize(read_count = n())
 
@@ -122,30 +100,6 @@ Nterm_FullLink_onTar_p <-
 
 Nterm_FullLink_onTar_p
 
-##
-Nterm_FullLink_onTar_violin_p <-
-  ggplot(data = Nterm_FullLinker_DF, 
-         mapping = aes(x = onTarget, 
-                       y = counts)) +
-  geom_violin(fill = "black") +
-  theme(
-    panel.grid.major = element_blank(), 
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    axis.line.x = element_line(color ="black"),
-    axis.line.y = element_line(color ="black"),
-    axis.ticks = element_line(color = "black"),
-    axis.text.x.bottom = element_text(color = "black"),
-    axis.text.y.left = element_text(color = "black"),
-    axis.title.x.bottom = element_text(color = "black"),
-    axis.title.y.left = element_text(color = "black")) +
-  xlab("Insert On Target") +
-  ylab("counts") +
-  scale_y_log10()
-
-Nterm_FullLink_onTar_violin_p
-
-
 
 #### Make figures for Cterm ####
 
@@ -172,26 +126,3 @@ Cterm_FullLink_onTar_p <-
   ylab("Read percentage")
 
 Cterm_FullLink_onTar_p
-
-##
-Cterm_FullLink_onTar_violin_p <-
-  ggplot(data = Cterm_FullLinker_DF, 
-         mapping = aes(x = onTarget, 
-                       y = counts)) +
-  geom_violin(fill = "black") +
-  theme(
-    panel.grid.major = element_blank(), 
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    axis.line.x = element_line(color ="black"),
-    axis.line.y = element_line(color ="black"),
-    axis.ticks = element_line(color = "black"),
-    axis.text.x.bottom = element_text(color = "black"),
-    axis.text.y.left = element_text(color = "black"),
-    axis.title.x.bottom = element_text(color = "black"),
-    axis.title.y.left = element_text(color = "black")) +
-  xlab("Insert On Target") +
-  ylab("counts") +
-  scale_y_log10()
-
-Cterm_FullLink_onTar_violin_p
